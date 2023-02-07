6a99afb2354e08e31eed51899600726c505ae6b3
Jonas <jonas.hauenstein@outlook.de>
2023-02-07 12:21:26 +0100

    find element in Goodreads and add some text

---
+++ README.md
@@ -0,0 +1,2 @@
+##Booktitle translator
+Small Chrome Extension that detects Book Titles and displays the translated version
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,7 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+
+//Sucht die Seite nach Tags der diese Attribute und Attributswerte hat
+var test = document.querySelectorAll('[data-testid="bookTitle"]');
+//fügt diesem Tag einen Text an
+test[0].innerHTML = test[0].innerHTML + ' ein testitest';

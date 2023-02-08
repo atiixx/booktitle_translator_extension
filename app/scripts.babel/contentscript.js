e75e9d79a6ec4514664c6c9ea66f7e773e33e8ac
Jonas <jonas.hauenstein@outlook.de>
2023-02-08 16:14:08 +0100

    working for englisch to german

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -13,6 +13,30 @@ async function searchForBook(title, author) {
 }).catch(console.log('No german version found.'));
 }
 
+function isGerman(){
+  var word = 'German',
+    queue = [document.body],
+    curr
+;
+while (curr = queue.pop()) {
+    if (!curr.textContent.match(word)) continue;
+    for (var i = 0; i < curr.childNodes.length; ++i) {
+        switch (curr.childNodes[i].nodeType) {
+            case Node.TEXT_NODE : // 3
+                if (curr.childNodes[i].textContent.match(word)) {
+                   return true;
+             // you might want to end your search here.
+                }
+                break;
+            case Node.ELEMENT_NODE : // 1
+                queue.push(curr.childNodes[i]);
+                break;
+        }
+    }
+}
+return false;
+}
+
 
 function findGerman(data) {
   var gerArr = data.items.filter((item) => item.volumeInfo.language === 'de');
@@ -22,6 +46,7 @@ function findGerman(data) {
 
 
 //Sucht die Seite nach Tags der diese Attribute und Attributswerte hat
+if(!isGerman()){
 var title = document.querySelectorAll('[data-testid="bookTitle"]');
 var author = document.querySelectorAll('[data-testid="name"]');
 var ele, node, germanTitle;
@@ -33,4 +58,4 @@ ele.textContent ='🇩🇪 ' + germanTitle;
 //fügt diesem Tag einen Text an
 title[0].parentNode.appendChild(ele);
 }).catch(console.log('Couldn\'t attach element'));
-
+};
